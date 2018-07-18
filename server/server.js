1348b27293e598b321b545633d915eab3d48b105
justice roy rubi <justice_rubi@yahoo.com>
2018-07-18 09:02:05 +0800

    Add connect and disconnect event handlers'

--- server/server.js
+++ server/server.js
@@ -7,21 +7,36 @@
 //    res.end('Hello World\n');
 //}).listen(port);
 
+const http = require('http');
 const express = require('express');
 const path = require('path');
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
+
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-    //console.log(__dirname + '/../public');
-    //console.log(publicPath);
-    res.render('index.html');
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected.');
+    });
 });
 
-app.listen(port, () => {
+//app.get('/', (req, res) => {
+//    //console.log(__dirname + '/../public');
+//    //console.log(publicPath);
+//    res.render('index.html');
+//});
+
+server.listen(port, () => {
     console.log(`Listening to port ${port}`);
 });
 
